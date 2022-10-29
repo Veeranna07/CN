484f93ada0c35004e865f3351bd8e8e73c459431
BHUKYA VEERANNA <104159515+Veeranna07@users.noreply.github.com>
2022-10-29 19:01:07 +0530

    Update RSA.java

--- RSA/RSA.java
+++ RSA/RSA.java
@@ -1,63 +1,77 @@
 package week12;
-//Java Program to Implement the RSA Algorithm
-import java.math.*;
-//import java.util.*;
 
-class RSA {
-	public static void main(String args[])
-	{
-		int p, q, n, z, d = 0, e, i;
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.math.BigInteger;
+import java.util.Random; 
 
-		// The number to be encrypted and decrypted
-		int msg = 12;
-		double c;
-		BigInteger msgback;
-
-		// 1st prime number p
-		p = 3;
-
-		// 2nd prime number q
-		q = 11;
-		n = p * q;
-		z = (p - 1) * (q - 1);
-		System.out.println("the value of z = " + z);
-
-		for (e = 2; e < z; e++) {
-
-			// e is for public key exponent
-			if (gcd(e, z) == 1) {
-				break;
-			}
-		}
-		System.out.println("the value of e = " + e);
-		for (i = 0; i <= 9; i++) {
-			int x = 1 + (i * z);
-
-			// d is for private key exponent
-			if (x % e == 0) {
-				d = x / e;
-				break;
-			}
-		}
-		System.out.println("the value of d = " + d);
-		c = (Math.pow(msg, e)) % n;
-		System.out.println("Encrypted message is : " + c);
-
-		// converting int value of n to BigInteger
-		BigInteger N = BigInteger.valueOf(n);
-
-		// converting float value of c to BigInteger
-		BigInteger C = BigDecimal.valueOf(c).toBigInteger();
-		msgback = (C.pow(d)).mod(N);
-		System.out.println("Decrypted message is : "
-						+ msgback);
-	}
-
-	static int gcd(int e, int z)
-	{
-		if (e == 0)
-			return z;
-		else
-			return gcd(z % e, e);
-	}
+public class RSA
+{
+ private BigInteger p;
+ private BigInteger q;
+ private BigInteger N;
+ private BigInteger phi;
+ private BigInteger e;
+ private BigInteger d;
+ private int bitlength = 1024;
+ private Random r;
+ 
+ public RSA()
+ {
+ r = new Random();
+ p = BigInteger.probablePrime(bitlength, r);
+ q = BigInteger.probablePrime(bitlength, r);
+ N = p.multiply(q);
+ phi = p.subtract(BigInteger.ONE).multiply(q.subtract(BigInteger.ONE));
+ e = BigInteger.probablePrime(bitlength / 2, r);
+ 
+while (phi.gcd(e).compareTo(BigInteger.ONE) > 0 && e.compareTo(phi) < 0)
+ {
+ e.add(BigInteger.ONE);
+ }
+ d = e.modInverse(phi);
+ }
+ public RSA(BigInteger e, BigInteger d, BigInteger N)
+ {
+ this.e = e;
+ this.d = d;
+ this.N = N;
+ }
+ @SuppressWarnings("deprecation")
+ public static void main(String[] args) throws IOException
+ {
+ RSA rsa = new RSA();
+ DataInputStream in = new DataInputStream(System.in);
+ String teststring;
+System.out.println("Enter the plain text:");
+ teststring = in.readLine();
+ System.out.println("Encrypting String: " + teststring);
+ System.out.println("String in Bytes: "
+ + bytesToString(teststring.getBytes()));
+// encrypt
+ byte[] encrypted = rsa.encrypt(teststring.getBytes());
+ // decrypt
+ byte[] decrypted = rsa.decrypt(encrypted);
+ System.out.println("Decrypting Bytes: " + bytesToString(decrypted));
+ System.out.println("Decrypted String: " + new String(decrypted));
+ }
+ private static String bytesToString(byte[] encrypted)
+ {
+ String test = "";
+ for (byte b : encrypted)
+ {
+ test += Byte.toString(b);
+ }
+ return test;
+ }
+ // Encrypt message
+ public byte[] encrypt(byte[] message)
+{
+ return (new BigInteger(message)).modPow(e, N).toByteArray();
+ }
+ // Decrypt message
+ public byte[] decrypt(byte[] message)
+ {
+ return (new BigInteger(message)).modPow(d, N).toByteArray();
+ }
 }
